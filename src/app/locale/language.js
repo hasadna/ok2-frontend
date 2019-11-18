e63855e4061024600354352b4dd4f2aad288ba27
alonrbar <alon.r.bar@gmail.com>
2019-11-18 22:32:39 +0200

    dir structure

---
+++ src/app/locale/language.js
@@ -0,0 +1,12 @@
+export const language = {
+    en: {
+        message: {
+            hello: 'hello world'
+        }
+    },
+    ja: {
+        message: {
+            hello: 'こんにちは、世界'
+        }
+    }
+}
