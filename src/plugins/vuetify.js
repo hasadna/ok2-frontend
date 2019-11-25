23e9a224a4d2b61b2dabeb56cb26b20f71a14237
Noga Shemer <nogashemer@gmail.com>
2019-11-25 22:48:56 +0200

    theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    rtl: true,
+    theme: {
+        themes: {
+            light: {
+                primary: '#757282',
+                secondary: '#b0bec5',
+                accent: '#8c9eff',
+                error: '#b71c1c',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FFC107'
+            },
+        },
+    }
 });
