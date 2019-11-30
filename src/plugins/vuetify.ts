b36cf42c96c52f005ac1e9ec333894bb11421f95
alonrbar <alon.r.bar@gmail.com>
2019-11-30 21:19:53 +0200

    restore editorconfig

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,18 +6,18 @@ import Vuetify from 'vuetify'; // eslint-disable-line
 Vue.use(VuetifyLib);
 
 export default new VuetifyLib({
-    rtl: true,
-    theme: {
-        themes: {
-            light: {
-                primary: '#757282',
-                secondary: '#b0bec5',
-                accent: '#8c9eff',
-                error: '#b71c1c',
-                info: '#2196F3',
-                success: '#4CAF50',
-                warning: '#FFC107'
-            },
-        },
-    }
+  rtl: true,
+  theme: {
+    themes: {
+      light: {
+        primary: '#757282',
+        secondary: '#b0bec5',
+        accent: '#8c9eff',
+        error: '#b71c1c',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      },
+    },
+  }
 });
