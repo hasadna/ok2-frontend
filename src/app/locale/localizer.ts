57ba7f4c48ff1ec785cc0e55ccdf50aa067a402e
alonrbar <alon.r.bar@gmail.com>
2019-12-02 22:18:16 +0200

    Localizer class done

--- src/app/locale/localizer.ts
+++ src/app/locale/localizer.ts
@@ -1,32 +1,44 @@
+import * as cookies from 'js-cookie';
+import Vue from 'vue';
+import { language } from './language';
 
 export class Localizer {
 
+  public static defaultLanguage = 'he';
+
+  public static instance = new Localizer(language);
+
   /**
-   * Name of the current active language.  
+   * Name of the current active language.
    *
    * Note that we sometimes use simple language name and not the full tag name.
    * For instance, our 'en' language name is in fact the 'en-US' standard
    * language tag.
    *
-   * More about language tags can be found here: 
+   * More about language tags can be found here:
    * - https://www.w3.org/International/articles/language-tags/
    * - https://r12a.github.io/app-subtags/
    */
-  public currentLanguageName = 'he';
+  public get languageName(): string {
+    return cookies.get('locale') || Localizer.defaultLanguage;
+  }
+  public set languageName(value: string) {
+    cookies.set('locale', value);
+  }
 
   /**
-   * The current language is a nested dictionary with this structure:  
-   * [module_name][phrase_name] = [localized_string]  
-   * 
+   * The current language is a nested dictionary with this structure:
+   * [module_name][phrase_name] = [localized_string]
+   *
    * Example: currentLanguage.common.appName (returns "כנסת פתוחה 2.0")
    */
-  public get currentLanguage(): IMap<IMap<string>> {
-    return this.langs[this.currentLanguageName];
+  public get language(): IMap<IMap<string>> {
+    return this.langs[this.languageName];
   }
 
   public get supportedLanguages(): string[] {
     return Object.keys(this.langs);
-  }  
+  }
 
   /**
    * [language_name][module_name][phrase_name]
@@ -36,8 +48,9 @@ export class Localizer {
   /**
    * @param langFile [module_name][phrase_name][language_name]
    */
-  constructor(langFile: IMap<IMap<IMap<string>>>) {
+  private constructor(langFile: IMap<IMap<IMap<string>>>) {
     this.langs = this.createLangsDictionary(langFile);
+    this.patchVue();
   }
 
   /**
@@ -65,5 +78,16 @@ export class Localizer {
     }
 
     return result;
+  }
+
+  /**
+   * Add the `language` property of this localizer instance as a Vue property.
+   *
+   * @see https://vuejs.org/v2/cookbook/adding-instance-properties.html
+   */
+  private patchVue() {
+    Object.defineProperty(Vue.prototype, '$language', {
+      get: () => this.language
+    });
   }
 }
