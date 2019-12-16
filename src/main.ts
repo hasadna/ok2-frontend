04e59050392318b54bbfb3c4a7c0fa1cdeb09d4f
Noga Shemer <nogashemer@gmail.com>
2019-12-16 19:20:48 +0200

    start routing

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
+import routes from './app/routes'
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app');
