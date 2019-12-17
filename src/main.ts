46e6cd3f20396a6e5325ff401cfda556d3cddecc
naaman <naaman@shieldfc.com>
2019-12-17 14:02:13 +0200

    fix lint

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import VueRouter from 'vue-router';
 import routes from './app/routes';
-import Vuex from 'vuex'
-import { store } from './store'
+import Vuex from 'vuex';
+import { store } from './store';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const router = new VueRouter({
   routes
