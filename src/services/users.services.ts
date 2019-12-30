8065c87eac280921a1a6f749981ae8f287d0669f
naaman <naaman@shieldfc.com>
2019-12-30 13:51:21 +0200

    add function return type

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,6 +1,7 @@
+import { User } from './../store/modules/user/index';
 
 class UsersService {
-  public async  getUser(username: string) {
+  public async  getUser(username: string): Promise<User> {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${1}`);
     const json = await res.json();
     return {
