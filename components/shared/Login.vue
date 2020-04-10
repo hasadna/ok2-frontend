eccd9dba6c12e547cbe74d710ba03013939efabf
naaman <naaman@shieldfc.com>
2020-04-10 11:05:12 +0300

    add sign in form

--- components/shared/Login.vue
+++ components/shared/Login.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-dialog v-model="dialog" persistent max-width="600px">
+  <v-dialog v-model="dialog" persistent max-width="350px">
     <v-card>
       <v-form id="login-form" @submit.prevent="login()">
         <v-card class="elevation-12">
-          <v-toolbar dark color="blue">
-            <v-toolbar-title>התחברות</v-toolbar-title>
+          <v-toolbar color="#FFFFFF" flat>
+            <v-toolbar-title>התחברות לכנסת פתוחה</v-toolbar-title>
             <v-spacer />
             <v-btn text @click="dialogState(false)">
               <v-icon>mdi-close</v-icon>
@@ -14,33 +14,53 @@
             <small>{{ errorMeesge }}</small>
           </v-alert>
           <v-card-text>
-            <v-text-field
+            <ok-input
               v-model="email"
               name="login"
               label="מייל / שם משתמש"
               type="text"
+              class="mb-4"
             />
-
-            <v-text-field
+            <ok-input
               v-model="password"
               name="password"
               label="סיסמא"
               type="password"
+              class="mb-4"
             />
           </v-card-text>
-          <v-divider light />
           <v-card-actions>
+            <v-btn
+              type="cancel"
+              class="mr-auto ml-2"
+              color="#FAFAFA"
+              large
+              @click="dialogState(false)"
+            >
+              ביטול
+            </v-btn>
+            <v-btn
+              type="submit"
+              form="login-form"
+              color="#4D4D4D"
+              class="white--text"
+              large
+            >
+              כניסה
+            </v-btn>
+          </v-card-actions>
+          <v-divider light />
+          <div
+            class="
+              flex"
+          >
             <v-btn
               type="button"
               @click.prevent="navigateToRegisterPage()"
             >
               הרשמה
             </v-btn>
-            <v-spacer />
-            <v-btn color="primary" dark type="submit" form="login-form">
-              התחברות
-            </v-btn>
-          </v-card-actions>
+          </div>
         </v-card>
       </v-form>
     </v-card>
@@ -48,8 +68,13 @@
 </template>
 <script>
 import { EventBus, BUSEVENTS } from '../../services/bus/bus';
+import OkInput from '~/components/shared/form/OkInput.vue';
+
 export default {
   name: 'Login',
+  components: {
+    OkInput
+  },
   props: {
     dialog: {
       type: Boolean,
---
+++ components/shared/form/OkInput.vue
@@ -0,0 +1,54 @@
+<template>
+  <label class="label ok-input">
+    <span class="mb-2 ok-input__title">
+      {{ label }}
+    </span>
+    <input :type="type" :name="name" class="input" @input="$emit('input', $event.target.value)">
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'OkInput',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    name: {
+      type: String,
+      required: true
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.label{
+  display:block;
+}
+.ok-input{
+  &__title{
+    width:100%;
+    color: #6F6F6F;
+    font-size: 14px;
+    line-height: 20px;
+  }
+  .input{
+    display:block;
+    width: 100%;
+    background: #D3D3D3;
+    border: 1px solid #A3A3A3;
+    box-sizing: border-box;
+    border-radius: 4px;
+    height: 40px;
+    padding: 0 4px;
+  }
+}
+
+</style>
+
